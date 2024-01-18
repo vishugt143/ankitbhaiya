159b3c1db80b2fbccf812954e2483395d76e5772
Dke76 <156434263+Dke76@users.noreply.github.com>
2024-01-18 21:13:30 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# txt uploader
+
+# Made By PARADISE
+
+
+## DEPLOY TO HEROKU
+
+
+[![Deploy to heroku chacha](https://www.herokucdn.com/deploy/button.svg)](https://dashboard.heroku.com/new?template=https://github.com/Chutiyahokaa/video5555)
---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+API_ID = API_ID = 20346550
+
+API_HASH = os.environ.get("API_HASH", "bc79c3bea7a626887bdc0871eecf0327")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6262497675:AAFLEXHAq_uw8np_dwaid05ViNTafux0hCs")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 1358747363))
+
+LOG = -1002027038775
+
+try:
+    ADMINS=[]
+    for x in (os.environ.get("ADMINS", "5665231556").split()):
+        ADMINS.append(int(x))
+except ValueError:
+        raise Exception("Your Admins list does not contain valid integers.")
+ADMINS.append(OWNER)
+
+
