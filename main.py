ed3c5c0e4f7c7998de6ab18c0e5c3eaa5193403f
Dke76 <156434263+Dke76@users.noreply.github.com>
2024-01-27 17:06:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ bot = Client("bot",
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text(f"HELLO I AM TXT DOWNLOADER BOT MADE BY Deepak[{m.from_user.first_name}](tg://user?id={m.from_user.id})\nPress /Deepak")
+    editable = await m.reply_text(f"HELLO I AM TXT DOWNLOADER BOT MADE BY Deepak[{m.from_user.first_name}](tg://user?id={m.from_user.id})\nPress /Ak47")
 
 
 @bot.on_message(filters.command("stop") & filters.user(ADMINS))
